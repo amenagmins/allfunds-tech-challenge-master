de13489a1aa420f4e4513ba6eb0c900eccfab8d9
Alejandro Mena González <amenag@minsait.com>
2024-05-21 14:14:50 +0200

    test: fix test

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
+import { ProductType } from "./types/commonTypes";
+import App from "./App";
+
+const productList: ProductType[] = [
+  {
+    id: "41fd4fd9-95c7-4809-96db-a147d352fdbb",
+    image_url:
+      "https://dummyimage.com/400x400/28200e/000&text=Unbranded Metal Chair",
+    stock: 8,
+    productName: "Unbranded Metal Chair",
+    price: 43,
+    productDescription:
+      "Porro tempore autem. Sunt molestias qui quod recusandae nemo quia optio. Nostrum aperiam officiis aut reprehenderit illo.",
+    favorite: false,
+  },
+  {
+    id: "20cc33f1-223b-4cf0-878d-fdedb3f60b56",
+    image_url:
+      "https://dummyimage.com/400x400/2ee9f7/000&text=Handcrafted Metal Towels",
+    stock: 41,
+    productName: "Handcrafted Metal Towels",
+    price: 98,
+    productDescription:
+      "Rerum minima laudantium blanditiis dolorem dolores ut sint ut quidem. Est doloremque repellat excepturi dolor consequatur rerum qui. Facere ut vel et enim accusamus ipsum dolores aut. Eaque quo ut omnis unde quam error voluptas non iure.",
+    favorite: false,
+  },
+];
+
+jest.mock("./api.ts", () => ({
+  getProductsList: () => Promise.resolve(productList),
+}));
+describe("Home tests", () => {
+  test("should render a list with products", async () => {
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getByText("Unbranded Metal Chair")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Handcrafted Metal Towels")).toBeInTheDocument();
+    });
+  });
+
+  test("Adding to cart should work correctly", async () => {
+    render(<App />);
+    await waitFor(() => {
+      const product = screen.getByText("Unbranded Metal Chair").closest("div");
+      if (product) {
+        const addToCartButton = within(product).getByRole("button", {
+          name: /Añadir al carrito/i,
+        });
+        fireEvent.click(addToCartButton);
+        fireEvent.click(addToCartButton);
+      }
+      expect(screen.getByText("86€")).toBeInTheDocument();
+    });
+  });
 });
